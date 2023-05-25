9bd55b84998c436bf29a3992e82aa0827e685066
JisinKeo <tntjd9514@naver.com>
2023-05-25 19:07:04 +0900

    Feat new block

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import turtle as t
 import random as r
+import time
+
+
+class Brick():
+    def __init__(self):
+        self.y = 0
+        self.x = 6
+        self.color = r.randint(1, 6)
+
 
 def draw_grid(block, grid):
+    block.clear()
     top = 250
     left = -150
     colors = ['black', 'red', 'blue', 'orange', 'yellow', 'green', 'purple', 'white']
     for y in range(len(grid)):
         for x in range(len(grid[0])):
-            sc_x = left + (x * 20)
-            sc_y = top - (y * 20)
+            sc_x = left + (x * 22)
+            sc_y = top - (y * 22)
             block.goto(sc_x, sc_y)
             block.color(colors[grid[y][x]])
             block.stamp()
@@ -16,6 +26,7 @@ def draw_grid(block, grid):
 
 if __name__ == "__main__":
     sc = t.Screen()
+    sc.tracer(False)
     sc.bgcolor("black")
     sc.setup(width=600, height=700)
     grid = [[0]*12 for _ in range(24)]
@@ -25,14 +36,29 @@ if __name__ == "__main__":
         grid[i].append(7)
     grid.append([7]*14)
 
+    for y in range(23, 20, -1):
+        for x in range(1, 13):
+            grid[y][x] = r.randint(1, 6)
     block = t.Turtle()
     block.penup()
     block.speed(0)
-    block.shape("turtle")
+    block.shape("square")
     block.color("blue")
-    draw_grid(block, grid)
+    block.setundobuffer(None)
 
-    for x in grid:
-        print(x)
+    brick = Brick()
+    grid[brick.y][brick.x] = brick.color
+    draw_grid(block, grid)
+    while True:
+        sc.update()
+        if grid[brick.y+1][brick.x] == 0:
+            grid[brick.y][brick.x] = 0
+            brick.y += 1
+            grid[brick.y][brick.x] = brick.color
+        for x in grid:
+            print(x)
+        print()
+        draw_grid(block, grid)
+        time.sleep(0.05)
 
     sc.mainloop()
