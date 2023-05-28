7165df3fa48d04ad9ac3741a978d6be59b4bd1c7
JisinKeo <tntjd9514@naver.com>
2023-05-28 14:40:30 +0900

    Feat continual_remove

--- main.py
+++ main.py
@@ -71,6 +71,23 @@ def grid_update(grid, blank):
                 grid[tmp_y-1][x] = 0
 
 
+def continual_remove():
+    global blank, ch
+    while True:
+        flag = 1
+        for y in range(23, 15, -1):
+            for x in range(1, 13):
+                if grid[y][x] != 0:
+                    ch = [[0] * 14 for _ in range(25)]
+                    blank = []
+                    DFS(y, x, grid, grid[y][x])
+                    if len(blank) >= 4:
+                        grid_update(grid, blank)
+                        flag = 0
+        draw_grid(block, grid)
+        if flag == 1:
+            break
+
 def game_over():
     pen.up()
     pen.goto(-120, 120)
@@ -130,11 +147,12 @@ if __name__ == "__main__":
             DFS(brick.y, brick.x, grid, brick.color)
             if len(blank) >= 4:
                 grid_update(grid, blank)
+                continual_remove()
             height = max_height(grid)
             if height <= 15:
                 game_over()
                 break
-            elif height >= 20:
+            elif height >= 22:
                 draw_grid(block, grid)
                 you_win()
                 break
